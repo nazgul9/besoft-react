0e58307fa1608bf8a0684561c4fa3eb4bb396232
Your Name <you@example.com>
2021-04-02 20:57:31 +0600

    fixed

--- 02-start/src/components/App.js
+++ 02-start/src/components/App.js
@@ -1,15 +1,50 @@
 import Header from "./Header" 
 import Body from "./Body"
+import { Component } from "react"
 
-const App = () => { // это функция - в React JS называется компонент
-    // const name = "Ratmir"; // эта переменная используется внизу
-    // const age = 17;
-    return (
-        <div>
-            <Header/>,
-            <Body/>
-        </div>
-    );
-};
+// const App = () => { // это функция - в React JS называется компонент
+//     // const name = "Ratmir"; // эта переменная используется внизу
+//     // const age = 17;
+//     return (
+//         <div>
+//             <Header/>,
+//             <Body/>
+//         </div>
+//     );
+// };
 
-export default App
+class App extends Component {
+
+    state = { 
+        arr: ["Hit the gym", "Pay bills", "Meet George", "Buy eggs", "Read a book", "Organize"]
+    }
+
+    deleteItem = (deletItem) =>{
+        this.setState(
+            (state) => {
+                const arr = state.arr.filter(
+                    (item) => item !==deletItem)
+                    return {arr}
+            })    
+    }
+
+    addItem = (item) => {
+        this.setState( // меняет state(состяние)
+            (state)=>{
+                const arr = [item,...state.arr] // ... озночает что вместо state, будет вставляться все его значения внутри
+                return {arr}
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <Header addItem={this.addItem} />
+                <Body deleteItem={this.deleteItem} arr = {this.state.arr} />    
+            </div>
+        )
+    }
+}
+
+export default App;
--- 02-start/src/components/Header.js
+++ 02-start/src/components/Header.js
@@ -1,30 +1,37 @@
 import {Component} from "react"
-const Header = () => {
-    return (
-        <div id="myDIV" className="header">
-            <h2>My To Do List</h2>
-            <input type="text" id="myInput" placeholder="Title..." />
-            <span className="addBtn">Add</span>
-        </div>
-    );
-};
 
-// class Header extends Component {
+class Header extends Component {
 
-//     state = {
+    state = {
+        ItemValue: "asan"
+    }
 
-//     }
+    OnInputChange = (e) => {
+        const input = e.target
 
-//     render(){
-//         return (
-//             <div id="myDIV" className="header">
-//                 <h2>My To Do List</h2>
-//                 <input type="text" id="myInput" placeholder="Title..." />
-//                 <span onClick={this.Add} className="addBtn">Add</span>
-//             </div>
-//         );
-//     }
-// }
+        this.setState({
+            ItemValue:input.value
+        })
+    }
 
+    onFormSubmit = (e) => {
+        e.preventDefault() // не делай свою работу(не перезагружай сайт)
+        if (this.state.ItemValue.trim() !== ""){
+            this.props.addItem(this.state.ItemValue)
+        }
+        this.state.ItemValue = ""
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.onFormSubmit} id="myDIV" className="header">
+                <h2>My To Do List</h2>
+                <input value={this.state.ItemValue} onChange={this.OnInputChange} type="text" id="myInput" placeholder="Title..." /> 
+                <button type="submit" className="addBtn">Add</button>
+            </form>
+            // onChange - это функция: "когда изменяется" - по словам агая 
+        )
+    }
+}
 
 export default Header // так мы разрешаем import

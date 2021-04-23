03bab2e36eff6a2807213af9be865ca320f74d20
Your Name <you@example.com>
2021-04-23 20:57:12 +0600

    23.04

--- 06-services my/src/app/App.js
+++ 06-services my/src/app/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios'
 import NewsItem from './NewsItem'
 import Navbar from '../components/Navbar'
-import {Route} from 'react-router-dom'
+import {Route} from "react-router-dom"
 import About from '../components/About'
 import Contacts from '../components/Contacts'
 import NewsDetail from '../components/NewsDetail'
@@ -10,16 +10,17 @@ import NewsDetail from '../components/NewsDetail'
 const App = () => {
 
     const [news, setNews] = useState([])
-    useEffect(()=>{
-        axios.get("https://nurkadyrnur.pythonanywhere.com/news/").then((res)=>{
+    
+    useEffect(() => {
+        axios.get("https://nurkadyrnur.pythonanywhere.com/news/").then((res) => {
             setNews(res.data.results);
         })
-    },[])
+    }, [])
 
     return (
         <div>
             <Navbar />
-            <div style={{borderRadius: "10px"}} className="container border shadow-sm">
+            <div style={{ borderRadius: "10px" }} className="container border shadow-sm">
                 <Route path="/" exact>
                     <h1 className="text-center mt-4">Hello news</h1>
                     <div className="row mt-5 mb-5">
@@ -30,10 +31,10 @@ const App = () => {
                         ))}
                     </div>
                 </Route>
-                <Route path = "/About" exact>
+                <Route path="/about" exact> 
                     <About />
                 </Route>
-                <Route path = "/Contacts" exact>
+                <Route path="/contacts" exact>
                     <Contacts />
                 </Route>
                 <Route path="/news/:newsId" exact>
---
+++ 08-library/src/app/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import Navbar from "../components/Navbar"
+
+const initalState = [
+  {
+    id: 1,
+    title: 'Evgeny Onegin',
+    price: 300,
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae esse quia culpa! Similique cumque harum optio impedit et eligendi, eius consectetur vel minus quisquam maxime ipsum. Porro, beatae unde?",
+    image: 'https://i.grenka.ua/shop/1/6/309/evgenij-onegin_dbc.jpg'
+  },
+  {
+    id: 2,
+    title: 'Evgeny Onegin',
+    price: 240,
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae esse quia culpa! Similique cumque harum optio impedit et eligendi, eius consectetur vel minus quisquam maxime ipsum. Porro, beatae unde?",
+    image: 'https://s1.livelib.ru/boocover/1004719195/o/6881/Aleksandr_Pushkin__Evgenij_Onegin.jpeg'
+  },
+  {
+    id: 3,
+    title: 'Evgeny Onegin',
+    price: 1000000,
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae esse quia culpa! Similique cumque harum optio impedit et eligendi, eius consectetur vel minus quisquam maxime ipsum. Porro, beatae unde?",
+    image: 'https://s1.livelib.ru/boocover/1002064587/200x305/a99a/boocover.jpg'
+  },
+  {
+    id: 4,
+    title: 'Evgeny Onegin',
+    price: 10,
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae esse quia culpa! Similique cumque harum optio impedit et eligendi, eius consectetur vel minus quisquam maxime ipsum. Porro, beatae unde?",
+    image: 'https://all-the-books.ru/upload/resize_cache/iblock/3e7/400_596_14ea206e1adc774c374d982cab5ef0488/evgeniy_onegin.jpg'
+  },
+]
+
+const App = () => {
+
+  const [books, setBooks] = useState(initalState)
+
+  return (
+    <div>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <div className="container">
+          <h1 className="text-center">Books</h1>
+        </div>
+        <div className="container border shadow-sm rounded p-4">
+          <div className="row">
+            {books.map((item) => (
+              <div className="col-3">
+                <div className="card books-item">
+                  <img className="card-img-top" src={item.image} alt=""></img>
+                  <div className="card-body">
+                    <h2 className="card-title">{item.title}</h2>
+                    <p className="card-text">Price: {item.price} som</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
